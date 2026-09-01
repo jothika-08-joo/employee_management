e46e373e9e576b521978ccec7f780a6712a515da
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-09-01 18:16:32 +0530

    created post and put api endpoints

--- app.js
+++ app.js
@@ -29,6 +29,56 @@ app.get("/employees/:id",async(req,res)=>{
         res.status(500).json({message:"database error"});
     }
 });
+app.post("/employees",async(req,res)=>{
+    try{
+        const {name,email,department,salary}=req.body
+        
+            if (!name || !email || !department || !salary){
+                return res.status(400).json({
+                    message:"all fields are required"
+                });
+            }
+            const result=await pool.query(`insert into employees (name,email,department,salary) values($1,$2,$3,$4)RETURNING *`,[name,email,department,salary]);
+            return res.status(201).json({
+                message:"employee created successfully",
+                employee:result.rows[0]
+            });
+        }catch(error){
+            console.log(error);
+            res.status(500).json({
+                message:"database error"
+            })
+        }
+
+});
+app.put("/employees/:id",async(req,res)=>{
+    try{
+        const id=req.params.id;
+        const{name,email,department,salary}=req.body;
+        if (!name || !email || !department || !salary){
+            return res.status(400).json({
+                message:"all fields are required"
+            });  
+        }
+        const result=await pool.query("update employees set name=$1,email=$2,department=$3,salary=$4 where id=$5 RETURNING *",[name,email,department,salary,id] );
+        if(result.rows.length===0){
+            return res.status(404).json({
+                message:"employee not found"
+            });
+        }
+        res.json({
+            message:"employee updated successfully",
+            employee:result.rows[0],
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            message:"database error",
+        });
+    }
+    
+
+});
 app.get("/",(req,res) => {
    res.send("employess management api is running ")
 });
