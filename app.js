a82ee126eafa2f52c84297cfa4629c4c5a4534eb
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-09-01 13:11:39 +0530

    added a middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app=express();
+app.use(express.json());
 const PORT=3000;
 app.get("/",(req,res) => {
    res.send("employess management api is running ")
@@ -9,10 +10,14 @@ app.get("/hello",(req,res)=>{
     res.send("hello from jothika")
 });
 
-app.get("/status",(req,res)=>{
+app.get("/status",(req,res)=>{ 
     res.json({status:"running"})
 })
 
+app.post("/employees",(req,res)=>{
+    console.log(req.body.name)
+    res.send(req.body);
+})
 
 app.listen(PORT,()=>{
    console.log(`server is running on port ${PORT}`);
