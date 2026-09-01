4444a21112b2187f1c6494d4da0cdb817557c0ae
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-09-01 16:36:27 +0530

    get employees route

--- app.js
+++ app.js
@@ -3,6 +3,32 @@ const express = require("express");
 const app=express();
 app.use(express.json());
 const PORT=3000;
+app.get("/employees",async (req,res)=>{
+    try{
+        const result=await pool.query("select * from employees");
+        res.json(result.rows);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            message:"internal server error",
+        });
+    }
+});
+app.get("/employees/:id",async(req,res)=>{
+    try{
+        const id=req.params.id;
+        const result=await pool.query("select * from employees where id=$1",[id]);
+        if (result.rows.length===0){
+            res.status(404).json({message:"employee not found"})
+        }else{
+            res.json(result.rows);
+        }
+        
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"database error"});
+    }
+});
 app.get("/",(req,res) => {
    res.send("employess management api is running ")
 });
@@ -15,10 +41,6 @@ app.get("/status",(req,res)=>{
     res.json({status:"running"})
 })
 
-app.post("/employees",(req,res)=>{
-    console.log(req.body.name)
-    res.send(req.body);
-})
 
 app.listen(PORT,()=>{
    console.log(`server is running on port ${PORT}`);
