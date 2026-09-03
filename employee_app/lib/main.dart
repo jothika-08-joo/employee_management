3873f3fa63a812b9cdfd933fa87e2ca87ea9f930
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-09-03 14:42:00 +0530

    Implemented TextFormField for form validation

--- employee_app/lib/main.dart
+++ employee_app/lib/main.dart
@@ -27,6 +27,7 @@ class _EmployeeScreen extends State<EmployeeScreen> {
   final emailController = TextEditingController();
   final departmentController = TextEditingController();
   final salaryController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -34,31 +35,81 @@ class _EmployeeScreen extends State<EmployeeScreen> {
       appBar: AppBar(title: const Text("Employee Management")),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Text("Name"),
-            TextField(controller: nameController),
-            SizedBox(height: 16),
-            Text("Email"),
-            TextField(controller: emailController),
-            SizedBox(height: 16),
-            Text("Department"),
-            TextField(controller: departmentController),
-            SizedBox(height: 16),
-            Text("salary"),
-            TextField(controller: salaryController),
-            SizedBox(height: 16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Text("Name"),
+              TextFormField(
+                controller: nameController,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "name is required";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              Text("Email"),
+              TextFormField(
+                controller: emailController,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "email is required";
+                  }
+                  if (!value.contains("@")) {
+                    return "enter a vaild email";
+                  }
+                  if (!value.contains(".")) {
+                    return "enter a vaild email";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              Text("Department"),
+              TextFormField(
+                controller: departmentController,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "department is required";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              Text("salary"),
+              TextFormField(
+                controller: salaryController,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "salary is required";
+                  }
+                  final num = int.tryParse(value.trim());
+                  if (num == null) {
+                    return "enter salary in number";
+                  }
+                  if (num <= 0) {
+                    return "salary must be greater than 0";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
 
-            ElevatedButton(
-              onPressed: () {
-                print(nameController.text);
-                print(emailController.text);
-                print(departmentController.text);
-                print(salaryController.text);
-              },
-              child: Text("add employee"),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    print(nameController.text);
+                    print(emailController.text);
+                    print(departmentController.text);
+                    print(salaryController.text);
+                  }
+                },
+                child: const Text("add employee"),
+              ),
+            ],
+          ),
         ),
       ),
     );
